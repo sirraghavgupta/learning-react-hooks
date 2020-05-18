e0b2065f89809488b6fb006251ede600b57a0223
raghavGuptaTTN <raghav.gupta@tothenew.com>
2020-05-18 14:05:31 +0530

    sending ingredients to firebase by fetch api

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -10,10 +10,33 @@ function Ingredients() {
   // need to use const here, unlike the class component where we directly
   // write the function name.
   const addIngredientHandler = (ingredient) => {
-    setIngredientsState((prevState) => [
-      ...prevState,
-      { id: Math.random().toString(), ...ingredient },
-    ]);
+    /**
+     * fetch is not a react feature, its inbuilt into the browser.
+     * it by default always makes a get request, so for post request, we need
+     * to configure that. initially axios used to do all this conf.
+     *
+     * we also, need to convert the object into the string so as to send it into
+     * the string.
+     * when we get the response, we also need to unpack it by using .json() on that.
+     * it also returns a promise, so we use another then block.
+     */
+    fetch(
+      "https://react-hooks-demo-app-b51ef.firebaseio.com/ingredients.json",
+      {
+        method: "POST",
+        body: JSON.stringify(ingredient),
+        headers: { "Content-Type": "application/json" },
+      }
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseData) => {
+        setIngredientsState((prevState) => [
+          ...prevState,
+          { id: responseData.name, ...ingredient },
+        ]);
+      });
   };
 
   return (
